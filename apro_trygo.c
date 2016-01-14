d54c0bf160519577fb6b43c2580eb8c0e7f840a9
Szymon <szymon_112233@wp.pl>
2016-01-14 22:56:53 +0100

    1 wersja poroksymatora , nie działajaca ale dajaća punkt wyjścia

--- apro_trygo.c
+++ apro_trygo.c
@@ -6,100 +6,59 @@ void
 make_spl(points_t * pts, spline_t * spl)
 {
 
-	matrix_t       *eqs= NULL;
-	double         *x = pts->x;
-	double         *y = pts->y;
-	double		a = x[0];
-	double		b = x[pts->n - 1];
-	int		i, j, k;
-	int		nb = pts->n - 3 > 10 ? 10 : pts->n - 3;
-  char *nbEnv= getenv( "APPROX_BASE_SIZE" );
+    double         *x = pts->x;
+    double         *y = pts->y;
+    int		i, j, k, m n=pts->n;
+    double a0=0.0,a[n],b[n]
 
-	if( nbEnv != NULL && atoi( nbEnv ) > 0 )
-		nb = atoi( nbEnv );
+    m = (n-1)/2;
 
-	eqs = make_matrix(nb, nb + 1);
+    for(i=0; i<n ; i++)
+    {
+        a0+=y[i];
+    }
+    a0/=n;
 
-#ifdef DEBUG
-#define TESTBASE 500
-	{
-		FILE           *tst = fopen("debug_base_plot.txt", "w");
-		double		dx = (b - a) / (TESTBASE - 1);
-		for( j= 0; j < nb; j++ )
-			xfi( a, b, nb, j, tst );
-		for (i = 0; i < TESTBASE; i++) {
-			fprintf(tst, "%g", a + i * dx);
-			for (j = 0; j < nb; j++) {
-				fprintf(tst, " %g", fi  (a, b, nb, j, a + i * dx));
-				fprintf(tst, " %g", dfi (a, b, nb, j, a + i * dx));
-				fprintf(tst, " %g", d2fi(a, b, nb, j, a + i * dx));
-				fprintf(tst, " %g", d3fi(a, b, nb, j, a + i * dx));
-			}
-			fprintf(tst, "\n");
-		}
-		fclose(tst);
-	}
-#endif
+    for(i=0; i<m ; i++)
+    {
+        a[i]=0.0;
+        for(j=0; j<n ; j++)
+        {
+            a[i]+=y[j]*cos(2*M_PI*i*j/n);
+        }
+        a[i]*=2;
+        a[i]/=n;
 
-	for (j = 0; j < nb; j++) {
-		for (i = 0; i < nb; i++)
-			for (k = 0; k < pts->n; k++)
-				add_to_entry_matrix(eqs, j, i, fi(a, b, nb, i, x[k]) * fi(a, b, nb, j, x[k]));
+        b[i]=0.0;
+        for(j=0; j<n ; j++)
+        {
+            b[i]+=y[j]*sin(2*M_PI*i*j/n);
+        }
+        b[i]*=2;
+        b[i]/=n;
+    }
 
-		for (k = 0; k < pts->n; k++)
-			add_to_entry_matrix(eqs, j, nb, y[k] * fi(a, b, nb, j, x[k]));
-	}
 
-#ifdef DEBUG
-	write_matrix(eqs, stdout);
-#endif
 
-	if (piv_ge_solver(eqs)) {
-		spl->n = 0;
-		return;
-	}
-#ifdef DEBUG
-	write_matrix(eqs, stdout);
-#endif
 
-	if (alloc_spl(spl, nb) == 0) {
-		for (i = 0; i < spl->n; i++) {
-			double xx = spl->x[i] = a + i*(b-a)/(spl->n-1);
-			xx+= 10.0*DBL_EPSILON;  // zabezpieczenie przed ulokowaniem punktu w poprzednim przedziale
-			spl->f[i] = 0;
-			spl->f1[i] = 0;
-			spl->f2[i] = 0;
-			spl->f3[i] = 0;
-			for (k = 0; k < nb; k++) {
-				double		ck = get_entry_matrix(eqs, k, nb);
-				spl->f[i]  += ck * fi  (a, b, nb, k, xx);
-				spl->f1[i] += ck * dfi (a, b, nb, k, xx);
-				spl->f2[i] += ck * d2fi(a, b, nb, k, xx);
-				spl->f3[i] += ck * d3fi(a, b, nb, k, xx);
-			}
-		}
-	}
-
-#ifdef DEBUG
-	{
-		FILE           *tst = fopen("debug_spline_plot.txt", "w");
-		double		dx = (b - a) / (TESTBASE - 1);
-		for (i = 0; i < TESTBASE; i++) {
-			double yi= 0;
-			double dyi= 0;
-			double d2yi= 0;
-			double d3yi= 0;
-			double xi= a + i * dx;
-			for( k= 0; k < nb; k++ ) {
-							yi += get_entry_matrix(eqs, k, nb) * fi(a, b, nb, k, xi);
-							dyi += get_entry_matrix(eqs, k, nb) * dfi(a, b, nb, k, xi);
-							d2yi += get_entry_matrix(eqs, k, nb) * d2fi(a, b, nb, k, xi);
-							d3yi += get_entry_matrix(eqs, k, nb) * d3fi(a, b, nb, k, xi);
-			}
-			fprintf(tst, "%g %g %g %g %g\n", xi, yi, dyi, d2yi, d3yi );
-		}
-		fclose(tst);
-	}
-#endif
-
+    if (alloc_spl(spl, nb) == 0)
+    {
+        for(i = 0 ; i < n ; i++)
+        {
+            double xx = spl->x[i] = p + i*(k-p)/(m-1);
+            spl->f[i] = 0;
+            spl->f1[i] = 0;
+            spl->f2[i] = 0;
+            spl->f3[i] = 0;
+            for (j = 0 ; j < m ; j++)
+            {
+                dt = 2.0*M_PI*j/n;
+                spl->f[i] += a[j]*cos(dt*xx) + b[j]*sin(dt*xx);
+                spl->f1[i] += (-1)*dt*a[j]*sin(dt*xx) + dt*b[j]*cos(dt*xx);
+                spl->f2[i] += (-1)*dt*dt*a[j]*cos(dt*xx) + (-1)*dt*dt*b[j]*sin(dt*xx);
+                spl->f3[i] += pow(dt,3)*a[j]*sin(dt*xx) + (-1)*pow(dt,3)*b[j]*cos(dt*xx);
+            }
+        }
+    }
 }
+
